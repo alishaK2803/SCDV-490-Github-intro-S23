307244d733893d40363a0d364d5d55e31384d274
Alisha Kumari <alishakumari@Alishas-MacBook-Air.local>
2023-03-08 17:00:52 -0500

    Alisha Kumari

--- names.py
+++ names.py
@@ -2,6 +2,6 @@ names = {}
 names['Michael Cammarere'] = {"laptop": "Macbook Pro", "phone": "iPhone 14 Pro"}
 names['Michael Plekan'] = {"laptop": "Gateway", "phone": "Casio flipphone"} 
 names['Noah Franz'] = {"laptop": "HP Omen 16t-k000", "phone": "iPhone XR"} 
-names['Aliciah Cumaray'] = {"laptop": "Alienware", "phone": "Pixelated 9"} 
+names['Alisha Kumari'] = {"laptop": "MacBook Air", "phone": "iPhone"} 
 names['Mateo Laframcapitalofidaho'] = {"laptop": "Commodore64", "phone": "Samsung magallenic cloud"} 
 names['Blayck Reavers'] = {"laptop": "Lenovo adamantium", "phone": "Ayephone 17"} 
